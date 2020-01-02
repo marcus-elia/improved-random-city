838c6a02f6f826dad8253d8864fd53c4712c6290
marcus-elia <mselia@uvm.edu>
2020-01-02 09:45:55 -0500

    Added getters and setters and isScrolling boolean so we know when the player is scrolling the screen

--- src/GameManager.java
+++ src/GameManager.java
@@ -2,13 +2,18 @@ import java.awt.*;
 
 public class GameManager
 {
+    // The overall size of the window
     private int width;
     private int height;
+
+    // Keep track of whether the user is holding down a key
+    private boolean isScrolling;
 
     public GameManager(int inputWidth, int inputHeight)
     {
         width = inputWidth;
         height = inputHeight;
+        isScrolling = false;
     }
 
     public void tick()
@@ -19,5 +24,33 @@ public class GameManager
     public void render(Graphics2D g2d)
     {
 
+    }
+
+    // ==========================================
+    //
+    //                 Getters
+    //
+    // ==========================================
+    public int getWidth()
+    {
+        return width;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public boolean getIsScrolling()
+    {
+        return isScrolling;
+    }
+
+    // ==========================================
+    //
+    //                 Setters
+    //
+    // ==========================================
+    public void setIsScrolling(boolean input)
+    {
+        isScrolling = input;
     }
 }
