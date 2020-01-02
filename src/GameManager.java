ccf36a78d7c25e3a4df85899ee8252ed10be9c08
marcus-elia <mselia@uvm.edu>
2020-01-02 09:19:44 -0500

    Started the GameManager class with a width and a height

---
+++ src/GameManager.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+public class GameManager
+{
+    private int width;
+    private int height;
+
+    public GameManager(int inputWidth, int inputHeight)
+    {
+        width = inputWidth;
+        height = inputHeight;
+    }
+
+    public void tick()
+    {
+
+    }
+
+    public void render(Graphics2D g2d)
+    {
+
+    }
+}
