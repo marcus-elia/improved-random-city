a0c06dd706ba6d158dd929f67e12165d5d2cf308
marcus-elia <mselia@uvm.edu>
2020-01-02 16:26:07 -0500

    Created a basic Intersection class with a LinkedList of Roads and an ArrayList of neighbor Intersections

---
+++ src/Intersection.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Intersection extends GameObject
+{
+    // Roads are stored in order by the angle they leave this intersection at, from
+    // least to greatest, with 0 being directly to the right, and going clockwise.
+    private LinkedList<Road> roads;
+
+    private RoadMap rm;
+
+    // Used to fill in the intersection
+    private ArrayList<Double> intersectionFillXPoints;
+    private ArrayList<Double> intersectionFillYPoints;
+    private Path2D intersectionFill;
+
+    // a list of the intersections directly connected to this by a road
+    private ArrayList<Intersection> neighbors;
+
+    public Intersection(GameManager inputManager, Point inputCenter, RoadMap inputRM)
+    {
+        super(inputManager, inputCenter);
+        rm = inputRM;
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics2D g2d) {
+
+    }
+
+    // ==========================================
+    //
+    //                 Getters
+    //
+    // ==========================================
+    public LinkedList<Road> getRoads()
+    {
+        return roads;
+    }
+    public ArrayList<Intersection> getNeighbors()
+    {
+        return neighbors;
+    }
+}
