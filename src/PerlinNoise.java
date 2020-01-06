2bc26ab71ebc54f0348411ce911d1b144ad88c2d
marcus-elia <mselia@uvm.edu>
2020-01-06 14:20:11 -0500

    Created a basic perlin noise function based on a youtube tutorial.

---
+++ src/PerlinNoise.java
@@ -0,0 +1,107 @@
+// This is based on code from https://www.youtube.com/watch?v=6-0UaeJBumA
+public class PerlinNoise
+{
+    private int outputWidth;
+    private int outputHeight;
+
+    private double[][] noiseSeed;
+    private double[][] perlinNoise;
+
+    public PerlinNoise()
+    {
+        outputWidth = 16;
+        outputHeight = 16;
+        noiseSeed = new double[outputWidth][outputHeight];
+        perlinNoise = new double[outputWidth][outputHeight];
+
+        this.fillNoiseSeed();
+    }
+
+
+    // ==========================================
+    //
+    //          Initialization Functions
+    //
+    // ==========================================
+    // Put random values between 0 and 1 in the seed array
+    public void fillNoiseSeed()
+    {
+        for(int i = 0; i < outputWidth; i++)
+        {
+            for(int j = 0; j < outputHeight; j++)
+            noiseSeed[i][j] = Math.random();
+        }
+    }
+
+
+
+    // ==========================================
+    //
+    //             Perlin Functions
+    //
+    // ==========================================
+    public double[] calculatePerlinNoise1D(int count, double[] seed, int numOctaves)
+    {
+        double[] output = new double[count];
+        for(int i = 0; i < count; i++)
+        {
+            double noise = 0;
+            double scale = 1;
+            int pitch = count;
+            double scaleSum = 0.0;
+            for(int oct = 0; oct < numOctaves; oct++)
+            {
+                int sample1 = (i / pitch) * pitch;
+                int sample2 = (sample1 + pitch) % count;
+
+                double blend = (i - sample1) / (double)pitch;
+                double sample = (1 - blend) * seed[sample1] + blend * seed[sample2];
+                noise += scale*sample;
+
+                pitch /= 2;
+
+                scaleSum += scale;
+                scale = scale / 2;
+            }
+            output[i] = noise / scaleSum;
+        }
+        return output;
+    }
+
+    public double[][] calculatePerlinNoise2D(int width, int height, double[][] seed, int numOctaves)
+    {
+        double[][] output = new double[width][height];
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                double noise = 0;
+                double scale = 1;
+                int pitch = width;
+                double scaleSum = 0.0;
+                for(int oct = 0; oct < numOctaves; oct++)
+                {
+                    int sampleX1 = (i / pitch) * pitch;
+                    int sampleY1 = (j / pitch) * pitch;
+
+                    int sampleX2 = (sampleX1 + pitch) % width;
+                    int sampleY2 = (sampleY1 + pitch) % width;
+
+                    double blendX = (i - sampleX1) / (double)pitch;
+                    double blendY = (j - sampleY1) / (double)pitch;
+                    double sampleT = (1 - blendX) * seed[sampleY1*width][sampleX1] + blendX * seed[sampleY1*width][sampleX2];
+                    double sampleB = (1 - blendX) * seed[sampleY2*width][sampleX1] + blendX * seed[sampleY2*width][sampleX2];
+
+                    noise += (blendY * (sampleB - sampleT) + sampleT) * scale;
+
+                    pitch /= 2;
+
+                    scaleSum += scale;
+                    scale = scale / 2;
+                }
+                output[i][j] = noise / scaleSum;
+            }
+        }
+        return output;
+    }
+}
