7e6a739478e77e091201784a7de5ca743a4b7747
marcus-elia <mselia@uvm.edu>
2020-01-07 09:19:25 -0500

    Created a MouseInput class to use clicks for debugging.

---
+++ src/MouseInput.java
@@ -0,0 +1,44 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+
+public class MouseInput implements MouseListener
+{
+
+    private GameManager manager;
+
+    public MouseInput(GameManager manager)
+    {
+        this.manager = manager;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e)
+    {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+        int mx = e.getX();
+        int my = e.getY();
+
+        manager.reactToClick(mx, my);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
