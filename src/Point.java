a3f567041b0af7befc79232e4a002865e341e0ae
marcus-elia <mselia@uvm.edu>
2020-01-08 08:53:46 -0500

    Added a midPoint() function

--- src/Point.java
+++ src/Point.java
@@ -72,4 +72,10 @@ public class Point
     {
         return new Point(p.x - x, p.y - y);
     }
+
+    // Returns the midpoint of two given points.
+    public static Point midPoint(Point p1, Point p2)
+    {
+        return new Point((p1.x + p2.x)/2, (p1.y + p2.y)/2);
+    }
 }
