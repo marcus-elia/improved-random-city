96c780e48dddf2e015a49ec0bc21cdcb638b7047
marcus-elia <mselia@uvm.edu>
2020-01-16 17:06:21 -0500

    Added functions to find the shortest path between two Intersections and return it as an ArrayList of Roads.

--- src/IntersectionGraph.java
+++ src/IntersectionGraph.java
@@ -57,4 +57,87 @@ public class IntersectionGraph
         }
     }
 
+    // Returns the Road connecting these two intersections, and null if no such Road exists.
+    public Road getConnectingRoad(Intersection intsec1, Intersection intsec2)
+    {
+        if(!hasEdge(intsec1, intsec2))
+        {
+            return null;
+        }
+
+        for(Road r : intsec1.getRoads())
+        {
+            if(r.goesBetweenIntersections(intsec1, intsec2))
+            {
+                return r;
+            }
+        }
+        System.out.println("No road goes between these intersections.");
+        return null;
+    }
+
+    // This applies Breadth First Search to find the shortest path of Intersections between start
+    // and end. The output is stored in reverse. This is just a helper method.
+    public ArrayList<Intersection> getShortestPathBackwards(Intersection start, Intersection end)
+    {
+        ArrayList<Intersection> output = new ArrayList<Intersection>();
+
+        // Keep track of which intersection led to each one we visit
+        HashMap<Intersection, Intersection> nodeToPrev = new HashMap<Intersection, Intersection>();
+        nodeToPrev.put(start, null);
+
+        // Keep track of the previous and current levels of nodes
+        ArrayList<Intersection> prevLevel;
+        ArrayList<Intersection> curLevel;
+
+        curLevel = new ArrayList<Intersection>();
+        curLevel.add(start);
+
+        boolean hasBeenFound = false;
+        while(!hasBeenFound)
+        {
+            prevLevel = curLevel;
+            curLevel = new ArrayList<Intersection>();
+            for(Intersection curInt : prevLevel)
+            {
+                // Look at each neighbor
+                for(Intersection intsec : edgeList.get(curInt))
+                {
+                    // If we haven't discovered it yet
+                    if(!nodeToPrev.containsKey(intsec))
+                    {
+                        nodeToPrev.put(intsec, curInt);
+                        curLevel.add(intsec);
+                    }
+                    if(intsec.equals(end))
+                    {
+                        hasBeenFound = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Trace backwards and add the path to the output
+        Intersection curInt = end;
+        while(curInt != null)
+        {
+            output.add(curInt);
+            curInt = nodeToPrev.get(curInt);
+        }
+        return output;
+    }
+
+    // Wrapper function
+    public ArrayList<Road> getShortestPath(Intersection start, Intersection end)
+    {
+        ArrayList<Intersection> backwardIntersections = getShortestPathBackwards(start, end);
+        ArrayList<Road> output = new ArrayList<Road>();
+        for(int i = backwardIntersections.size() - 1; i > 0; i--)
+        {
+            output.add(getConnectingRoad(backwardIntersections.get(i), backwardIntersections.get(i-1)));
+        }
+        return output;
+    }
+
 }
