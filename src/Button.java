5db636f022f59f75194ea04672260e2ecf7cad15
marcus-elia <mselia@uvm.edu>
2020-01-17 16:06:21 -0500

    Created a Button class that can be overridden and used.

---
+++ src/Button.java
@@ -0,0 +1,93 @@
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+public class Button
+{
+    private boolean isHighlighted;
+    private Point center;
+    private double xWidth;
+    private double yWidth;
+    private Shape rectangle;
+    private ButtonOutput output;
+    private GameManager manager;
+    private Color normalColor, highlightedColor;
+    private String word;
+    private int fontSize;
+
+    public Button(GameManager inputManager, Point inputCenter,  double inputXWidth, double inputYWidth,
+                  Color inputNormalColor, String inputWord, int inputFontSize, ButtonOutput inputOutput)
+    {
+        manager = inputManager;
+        center = inputCenter;
+        xWidth = inputXWidth;
+        yWidth = inputYWidth;
+        rectangle = new Rectangle2D.Double(center.x - xWidth/2, center.y - yWidth/2, xWidth, yWidth);
+        isHighlighted = false;
+        normalColor = inputNormalColor;
+        highlightedColor = new Color(normalColor.getRed(), normalColor.getGreen(), normalColor.getBlue(), 128);
+        word = inputWord;
+        fontSize = inputFontSize;
+        output = inputOutput;
+    }
+
+    public void render(Graphics2D g2d)
+    {
+        if(isHighlighted)
+        {
+            g2d.setColor(highlightedColor);
+        }
+        else
+        {
+            g2d.setColor(normalColor);
+        }
+        g2d.fill(rectangle);
+        this.drawWord(g2d);
+    }
+
+    // Draw the word in the middle of the rectangle
+    public void drawWord(Graphics2D g2d)
+    {
+        g2d.setFont(new Font("Tahoma", Font.BOLD, fontSize));
+        g2d.setColor(Color.BLACK);
+        int pixelLength = g2d.getFontMetrics().stringWidth(word); // the number of pixels the string is long
+        g2d.drawString(word, (int)center.x - pixelLength/2, (int)center.y + 5);
+    }
+
+    // ==========================================
+    //
+    //                Setters
+    //
+    // ==========================================
+    public void setIsHighlighted(boolean input)
+    {
+        isHighlighted = input;
+    }
+
+
+    // ==========================================
+    //
+    //           Reacting to the Mouse
+    //
+    // ==========================================
+    public void reactToMouseMovement(int mx, int my)
+    {
+        if(rectangle.contains(new Point2D.Double(mx, my)))
+        {
+            isHighlighted = true;
+        }
+        else
+        {
+            isHighlighted = false;
+        }
+    }
+
+    public ButtonOutput reactToMouseClick(int mx, int my)
+    {
+        if(rectangle.contains(new Point2D.Double(mx, my)))
+        {
+            return output;
+        }
+        return null;
+    }
+}
